5377caae3da0f47d7bb4a3d8815599f9aaadb64f
Abhijeet Singh Rana <dell1558laptop@gmail.com>
2022-09-14 19:39:25 +0530

    test

--- src/api.js
+++ src/api.js
@@ -5,17 +5,15 @@ export function getProductData(id){
 }
 
 export function getProductList(){
-  return axios.get("https://dummyjson.com/products/") //then(function(products){
-  //   return products.data.products
-  // })
+  return axios.get("https://dummyjson.com/products/")
 }
 
-// export default { getProductData, getProductList }
 
-// export const fakestore = async()=>{
-//   const response=await fetch("https://fakestoreapi.com/products")
-//   // return axios.get("https://fakestoreapi.com/products")
-//   console.log(response)
-//   const jsonData=await response.json
-//   console.log(jsonData)
-// }
+// export function getProductData(id){
+//   return  axios.get("https://fakestoreapi.com/products" + id)
+//  }
+ 
+//  export function getProductList(){
+//    return axios.get("https://fakestoreapi.com/products")
+//  }
+//  console.log(getProductList)
